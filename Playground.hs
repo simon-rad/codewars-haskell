157085eec591779cd3c1c097ef4b9e1148afa118
Wobbly-Wibbly <simon.radionov@gmail.com>
2020-12-18 12:08:00 +0200

    + set up cabal

--- Playground.hs
+++ Playground.hs
@@ -1,8 +1,37 @@
-module Codewars.Kata.FindOdd where
-import Data.Bits (xor)
--- | Given a list, find the [Int] that appears an 
---   odd number of times. The tests will always
---   provide such a number, and the list will
---   always contain at least one element.
-findOdd :: [Int] -> Int
-findOdd = foldr xor 0
+module Playground where
+
+import Data.Char
+import Data.List
+import Data.List.Split
+
+        
+-- demovingShift :: [String] -> Int -> String
+-- your code
+
+-- movingShift :: String -> Int -> [String]
+-- your code
+
+brakeString :: String -> [[(Int, Char)]]
+brakeString x = splitByIndex indexed by
+    where 
+        by = getDivider x + 1
+        indexed = zip [1..] x
+
+splitByIndex :: [(Int, Char)] -> Int -> [[(Int, Char)]]
+splitByIndex str i
+ | length str <= i = [str]
+ | otherwise = cResult : splitByIndex remaining i
+ where 
+     brakeF = \x -> fst x `mod` i == 0
+     broken = break brakeF str
+     cResult = fst broken
+     remaining = snd broken
+
+getDivider :: String -> Int
+getDivider x = ceiling ( fromIntegral (length x) / 5 ) :: Int
+        
+-- test :: Int -> Int
+source = "I should have known that you would have a perfect answer for me!!!"
+expected = ["J vltasl rlhr ","zdfog odxr ypw"," atasl rlhr p ","gwkzzyq zntyhv"," lvz wp!!!"]
+
+
---
+++ how-to-run-reminder.md
@@ -0,0 +1,12 @@
+# How to run the project
+
+## Cabal
+
+- `cabal v2-repl codewars-haskell`
+- run `cabal repl` and then `:load Playground.hs`
+
+## Stack
+
+Didn't manage to build with stack
+
+`stack build --file-watch --fast --pedantic`
